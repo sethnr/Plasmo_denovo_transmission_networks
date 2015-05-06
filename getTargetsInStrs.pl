0c12f99d3a14223e203442ae1f5ad488ef4a08db
Seth Redmond <sredmond@wmbe7-b78.broadinstitute.org>
2015-05-06 18:48:49 -0400

    parsing out mreps STRs in greenhouse locations

--- getTargetsInStrs.pl
+++ getTargetsInStrs.pl
@@ -3,19 +3,34 @@
 $targetFile = shift;
 $strFile = shift;
 
-open(STRS,$strfile);
+open(STRS,$strFile);
 open(TARGETS,$targetFile);
 
 while(<TARGETS>) {
-	      my $name, $chr, $st, $en = split(/\s/,$_);
-	      push @{$targets{$chr}}, \($name,$st,$en);
+#  print $_;
+  my ($name, $chr, $st, $en) = split(/\s+/,$_);
+  my @target = ($name,$st,$en);
+#  print "@target";
+#  print "$name, $chr, $st, $en !\n";
+  push @{$targets{lc($chr)}}, \@target;
 }
 
-print %targets;
+foreach $chr (keys %targets) {
+  foreach $target (@{$targets{$chr}}) {
+    print "\#".join("\t", $chr, @{$target})."\n";
+  }
+}
 
 while(<STRS>) {
-  my @F = split(/\s/,$_);
-  foreach $target @{$targets{$F[0]}} {
+  my @F = split(/\s+/,$_);
+#  print join("\t",$F[0], $F[1], $F[3],$_);
+  foreach $target (@{$targets{lc($F[0])}}) {
     ($name, $st, $en) = @{$target};
+    if ($F[1] < $en && $F[3] > $st) {
+      print "\n\#\#$name:$st-$en\n" unless $targeted{$name};
+      $targeted{$name} = 1;
+
+      print $_;
+    }
   }
 }
