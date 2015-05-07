30db8312c771981a8a8c043b52f695bc7c3d1839
Seth Redmond <sredmond@wmbe7-b78.broadinstitute.org>
2015-05-07 15:45:59 -0400

    prettify wrapper script

--- runDiscovarVar.sh
+++ runDiscovarVar.sh
@@ -8,28 +8,30 @@ REGION=$4
 #NAME=${SET}_${STR}_${LANE}
 NAME=${SET}_${LANE}
 
+echo "SETUP..."
 mkdir $NAME
 cd $NAME
 mkdir tmp
 
+echo "LINK/INDEX BAM..."
 FILE=`ls ${DISCODATA}/${LANE}/${SET}/*bam`
-
 ln -s $FILE ${NAME}.bam
 samtools index ${NAME}.bam
 
+echo "DISCOVAR"
 Discovar READS=${NAME}.bam \
 	 REGIONS=${REGION} \
 	 OUT_HEAD=${NAME} \
 	 TMP=./tmp \
 	 REFERENCE=${WORK}/refs/PlasmoDB-24_Pfalciparum3D7_Genome.fasta
 
-echo "DISCOVAR COMPLETE"
-rm -r tmp
-
+echo "MAKE OUTPUTS"
 dot -Tpng -o ${NAME}.final.png ${NAME}.final.dot
-
 perl -i -pe "s/${SET}$/${NAME}/gi" ${NAME}.final.variant.filtered.vcf
 bgzip ${NAME}.final.variant.filtered.vcf
 tabix ${NAME}.final.variant.filtered.vcf.gz
+
+echo "CLEANUP"
+rm -r tmp
 
 
