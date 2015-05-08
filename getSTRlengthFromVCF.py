8204e356bf7ba8a13a064d4a6244d37aab4fbf49
Seth Redmond <sredmond@wmbe7-b78.broadinstitute.org>
2015-05-08 13:35:24 -0400

    parse STR length from VCF

---
+++ getSTRlengthFromVCF.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import vcf
+import sys
+import argparse
+from string import *
+
+parser = argparse.ArgumentParser(description='get STR length differences from disco VCF')
+
+parser.add_argument('-v','--vcf', action="store", dest='vcfFile', type=str, help='vcfFile', nargs='?', default=None)
+parser.add_argument('-i','--defaultSize', action="store", dest='defSize', type=int, help='default size for STRs', nargs='?', default=0)
+
+
+args = parser.parse_args()
+print args.vcfFile
+
+vcfFile = open(args.vcfFile,'r')
+print vcfFile
+reader=vcf.Reader(vcfFile)
+print reader
+
+for record in reader:
+    refAllele = record.alleles[0]
+    STRlens = dict()
+    
+    for allele in record.alleles:
+        STRlen = len(allele) - len(refAllele) + args.defSize
+        STRlens[str(allele)] = str(STRlen)
+#    print record.alleles
+#    print STRlens
+    print "\t".join([str(record.CHROM),str(record.POS)]),
+    for call in record.samples:
+        callSTRlens = []
+        if call.gt_type is None:
+            print "\t.",
+        else:
+            print "\t",
+            for allele in split(call.gt_bases,"/"):
+                callSTRlens += [STRlens[allele]]
+            print "/".join(callSTRlens),
+    print ""
