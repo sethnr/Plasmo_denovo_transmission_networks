5184903c14704843aae188fe0722ea9a36d27987
Seth Redmond <sredmond@broadinstitute.org>
2015-05-26 17:56:55 -0400

    compared edges from 3D7/DD2 runs, got alignAndView (IGV) script running

--- alignAndView.sh
+++ alignAndView.sh
@@ -3,40 +3,51 @@
 
 ## defaults
 REF=$WORK/refs/Pf3D7_v3.fasta
+OUTDIR="alignViewOut"
 
-while getopts "f:p:" opt; do
+while getopts "f:p:o:" opt; do
   case $opt in
-      r) REF=${OPTARG}
-	 shift $((OPTIND-1));;
-      p) REGION=${OPTARG}
-	 shift $((OPTIND-1));;
+      r) REF=${OPTARG};;
+      p) REGION=${OPTARG};;
+      o) OUTDIR=${OPTARG};;
    \?) echo "Invalid option: -$OPTARG" >&2 ;;
   esac
 done
+shift $((OPTIND-1))
+
 OPTIND=1
 
 echo "aligning contigs with bwmem"
 
+if [ ! -d ${OUTDIR} ];
+then
+  mkdir ${OUTDIR}
+fi
+
+cd $OUTDIR
+
 for FASTA in "$@"
 do
-	perl  ~/bin/fasta_to_fastq.pl $FASTA > ${FASTA//.fasta/.fastq}
-	bwa mem  -SP  $REFERENCE \
-	 ${FASTA//.fasta/.fastq} \
+	perl  ~/bin/fasta_to_fastq.pl ../$FASTA > ${FASTA//.fasta/.fastq}
+	echo bwa mem  -SP $REF ${FASTA//.fasta/.fastq}
+	bwa mem  -SP $REF ${FASTA//.fasta/.fastq} \
 	 > ${FASTA//.fasta/.sam}
 	samindex ${FASTA//.fasta/.sam}
 done
 
-cat "new" > viewRegion.${REGION}.igv
-cat "genome "${REF} >> viewRegion.${REGION}.igv
-cat "goto "${REGION} >> viewRegion.${REGION}.igv
+echo "new" > viewRegion.${REGION}.igv
+echo "genome "${REF} >> viewRegion.${REGION}.igv
+echo "goto "${REGION} >> viewRegion.${REGION}.igv
 
 for FASTA in "$@"
 do
  echo "load " ${FASTA//.fasta/.bam} >> viewRegion.${REGION}.igv
 done
 
-cat "snapshot" >> viewRegion.${REGION}.igv
+echo "collapse" >> viewRegion.${REGION}.igv
+echo "snapshot "${OUTDIR}".png" >> viewRegion.${REGION}.igv
 
-use igv-2.3
+#use igv-2.3
 
-igv -b viewRegion.${REGION}.igv
+igv.sh -b viewRegion.${REGION}.igv
+
