578669be65f72aa2eed5544e5d2fc371a635755e
Seth Redmond <sredmond@node1749.broadinstitute.org>
2015-08-11 15:06:26 -0400

    dd2 concordance comparisons - with major/minor alleles compared

--- scripts/cfs_dd2/checkLaneConcordance.py
+++ scripts/cfs_dd2/checkLaneConcordance.py
@@ -4,10 +4,12 @@ import vcf
 import sys
 import argparse
 from string import *
-
+import collections
 parser = argparse.ArgumentParser(description='get STR length differences from disco VCF')
 
 parser.add_argument('-v','--vcf', action="store", dest='vcfFile1', type=str, help='vcfFile1', nargs='?', default=None)
+parser.add_argument('-s','--samples', action="append", dest='samples', type=str, help='samples to calc across', nargs='?', default=None)
+parser.add_argument('-p','--prefixVars', action="store", dest='prefix', type=str, help='samples to calc across', nargs='?', default="")
 
 parser.add_argument('-o','--out', action="store", dest='outFile', type=str, help='make combined outfile named <outfile> instead of separate files', nargs='?', default=None)
 #parser.add_argument('-i','--defaultSize', action="store", dest='defSize', type=int, help='default size for STRs', nargs='?', default=0)
@@ -18,18 +20,46 @@ args = parser.parse_args()
 vcfFile1 = open(args.vcfFile1,'r')
 reader1=vcf.Reader(vcfFile1)
 
-vcfoutF1 = replace(args.vcfFile1,'.vcf','.lanes.vcf')
-vcfoutF1 = replace(vcfoutF1,'.vcf.gz','.lanes.vcf')
+
+_Info = collections.namedtuple('Info', ['id', 'num', 'type', 'desc', 'source', 'version'])
+_Filter = collections.namedtuple('Filter', ['id', 'desc'])
+_Alt = collections.namedtuple('Alt', ['id', 'desc'])
+_Format = collections.namedtuple('Format', ['id', 'num', 'type', 'desc'])
+
+reader1.infos[args.prefix+'MISS'] = _Info(id=args.prefix+'MISS', num=1, type='Float', desc=args.prefix+' missingness', source="checkLaneConcordance.py", version=1)
+reader1.filters[args.prefix+'LaneErr'] = _Filter(id=args.prefix+'LaneErr', desc=args.prefix+' lanes not concordant')
+
+
+reader1.infos['ED'] = _Info(id='ED', num=1, type='Float', desc='Discovar mean edge depth across all samples', source="Discovar", version=1)
+reader1.infos['EDS'] = _Info(id='EDS', num=1, type='Float', desc='Discovar edge depth std dev across all samples', source="Discovar", version=1)
+
+
+# for i in reader1.infos:
+#    info = reader1.infos[i]
+#    print "INFO: "+i+" "+str(info)
+#for format in reader1.formats:
+#    print "FORMAT: "+format
+#for filt in reader1.filters:
+#    print "FILTER: "+filt
+
+
+vcfoutF1 = replace(args.vcfFile1,'.vcf','.'+args.prefix+'LCHK.vcf')
+vcfoutF1 = replace(vcfoutF1,'.vcf.gz','.'+args.prefix+'LCHK.vcf')
 print >>sys.stdout, "VCFOUT1",vcfoutF1
 vcfoutF1 = open(vcfoutF1,'w')
 vcfout1=vcf.Writer(vcfoutF1,reader1)
 
-samples = set()
-for s in reader1.samples:
-    sample, lane = s.split('_')
-    samples.add(sample)
-samples = list(samples)
-samples = sorted(samples)
+if args.samples is None:    
+    samples = set()
+    for s in reader1.samples:
+        sample, lane = s.split('_')
+        samples.add(sample)
+    samples = list(samples)
+    samples = sorted(samples)
+else:
+    samples = args.samples
+
+print samples
 
 print "\t".join(["chr","pos","alleles","type"]+samples)
 for rec in reader1:
@@ -38,22 +68,42 @@ for rec in reader1:
         sample, lane = call.sample.split('_')
         calls[(sample,int(lane))] = call.gt_type
 #    samples = list(set([s for s,l in calls]))
+    laneError = 0
+    missing = 0.0
     for s in samples:
         if (s,1) not in calls: calls[(s,1)] = None
         if (s,2) not in calls: calls[(s,2)] = None
 
         if calls[(s,1)] is None and calls[(s,2)] is None:
             calls[(s,-1)] = "."
+            missing += 2
         elif calls[(s,1)] is None:
             calls[(s,-1)] = str(calls[(s,2)])+"*"
+            missing += 1
         elif calls[(s,2)] is None:        
             calls[(s,-1)] = str(calls[(s,1)])+"*"
+            missing += 1
         elif calls[(s,1)] == calls[(s,2)]:
             calls[(s,-1)] = str(calls[(s,1)])
         else:
             calls[(s,-1)] = "!!"
-    if rec.is_indel:
-        print "\t".join([rec.CHROM,str(rec.POS),str(len(rec.alleles)),rec.var_subtype]+
-                        [calls[(s,-1)] for s in samples])
+            laneError +=1
+#    if rec.is_indel:
+#        print "\t".join([rec.CHROM,str(rec.POS),str(len(rec.alleles)),rec.var_subtype]+
+#                        [calls[(s,-1)] for s in samples])
 
+    rec.INFO[args.prefix+'MISS']=round(missing/len(rec.samples),4)
+    if laneError > 0:
+#        print len(rec.FILTER)
+        if len(rec.FILTER) == -1:
+            rec.FILTER[1]=args.prefix+'LaneErr'
+        else:
+            rec.FILTER += [args.prefix+'LaneErr']
 
+    if len(rec.ALT) > 50:
+        altp = reader1.formats.keys().index('ALTP')
+        print "\t".join([rec.CHROM,str(rec.POS),str(len(rec.ALT)),",".join(rec.FILTER)])
+#        for call in rec.samples:
+#            print call.data[altp]
+    else:
+        vcfout1.write_record(rec)
--- scripts/cfs_dd2/checkRealVFakeDD2.py
+++ scripts/cfs_dd2/checkRealVFakeDD2.py
@@ -4,15 +4,16 @@ import vcf
 import sys
 import argparse
 from string import *
-
-parser = argparse.ArgumentParser(description='get STR length differences from disco VCF')
+import collections
+parser = argparse.ArgumentParser(description='check for concordance with single-sample truth set (nucmer ref comparisons?)')
 
 parser.add_argument('-v1','--vreal', action="store", dest='vcfFile1', type=str, help='vcfFile1: real DD2', nargs='?', default=None)
 parser.add_argument('-v2','--vfake', action="store", dest='vcfFile2', type=str, help='vcfFile2: fake DD2', nargs='?', default=None)
 
-parser.add_argument('-o','--out', action="store", dest='outFile', type=str, help='make combined outfile named <outfile> instead of separate files', nargs='?', default=None)
-#parser.add_argument('-i','--defaultSize', action="store", dest='defSize', type=int, help='default size for STRs', nargs='?', default=0)
-
+parser.add_argument('-s','--samples', action="append", dest='samples', type=str, help='samples to calc across', nargs='?', default=None)
+parser.add_argument('-p','--prefixVars', action="store", dest='prefix', type=str, help='prefix for outfile / info / filters', nargs='?', default="DD2")
+parser.add_argument('-F','--noFilter', action="store_false", dest='filter', help='do not add filter if MAF > 1', default=True)
+parser.add_argument('-M','--ignoreMinors', action="store_true", dest='ignoreMinors', help='ignore minor allele calls when looking at concordance', default=False)
 
 args = parser.parse_args()
 
@@ -22,9 +23,21 @@ realReader=vcf.Reader(vcfFile1)
 vcfFile2 = open(args.vcfFile2,'r')
 fakeReader=vcf.Reader(vcfFile2)
 
-vcfoutF1 = replace(args.vcfFile1,'.vcf','.DD2c.vcf')
-vcfoutF1 = replace(vcfoutF1,'.vcf.gz','.DD2c.vcf')
+_Info = collections.namedtuple('Info', ['id', 'num', 'type', 'desc', 'source', 'version'])
+_Filter = collections.namedtuple('Filter', ['id', 'desc'])
+_Alt = collections.namedtuple('Alt', ['id', 'desc'])
+_Format = collections.namedtuple('Format', ['id', 'num', 'type', 'desc'])
+
+realReader.infos[args.prefix+'CONC'] = _Info(id=args.prefix+'CONC', num=1, type='Float', desc=args.prefix+' concordance with fake sample', source="checkSampleConcordance.py::"+args.vcfFile2, version=1)
+if args.filter:
+    realReader.filters[args.prefix+'Discord'] = _Filter(id=args.prefix+'Discord', desc=args.prefix+' found in DD2 ref, but allele not concordant')
+    realReader.filters[args.prefix+'RefAbsent'] = _Filter(id=args.prefix+'RefAbsent', desc=args.prefix+' var not found in DD2 ref')
+
+
+vcfoutF1 = replace(args.vcfFile1,'.vcf','.'+args.prefix+'CONC.vcf')
+vcfoutF1 = replace(vcfoutF1,'.vcf.gz','.'+args.prefix+'CONC.vcf')
 #print >>sys.stdout, "VCFOUT1",vcfoutF1
+
 vcfoutF1 = open(vcfoutF1,'w')
 vcfout1=vcf.Writer(vcfoutF1,realReader)
 
@@ -42,76 +55,120 @@ for rec in fakeReader:
     fakecall[var] = rec.REF+"/"+str(rec.ALT[0])
 #    print var+" "+rec.REF+"/"+str(rec.ALT[0])
 
-DD2s=["SM-7LV8O","SM-7LV8P","SM-7LV8Q","SM-7LV8R"]
+#DD2s=["SM-7LV8O","SM-7LV8P","SM-7LV8Q","SM-7LV8R"]
 
-print "\t".join(["chr","pos","alleles","type","conc","cons","all"]+samples)
+#print "\t".join(["chr","pos","alleles","type","conc","cons","all"]+samples)
+
+fCounts=dict()
+
 for rec in realReader:
     calls = dict()
 
     #calculate call concordance
     conc=None
     var = rec.CHROM+":"+str(rec.POS)
+    
+    alleles = dict()
+    alleles[rec.REF]=0
+    for alt in rec.ALT:
+        alleles[str(alt)]=0
+
+
+#    for call in rec.samples:
+#        sample, lane = call.sample.split('_')
+#        calls[(sample,int(lane))] = call.gt_bases
+    genos =0
+    missing = 0
+    
+    for sample in samples:
+        for lane in ('1','2'):
+            call = rec.genotype(sample+"_"+lane)
+            gt = call.gt_bases
+            if gt is not None:
+                a1, a2 = gt.split('/')
+                alleles[a1]+=1
+                genos+=1
+                alleles[a2]+=1
+                genos +=1
+            else:
+                missing +=1
+    major = ''
+    majorC = 0
+    
+    acopy = [a for a in alleles]
+    for a in acopy:
+        if alleles[a] == 0: del alleles[a] 
+        else:
+            if alleles[a] > majorC:
+                majorC = alleles[a]
+                major=a
+
+    conc = False
+    #if has any alt alleles (some can be refcalls only)
+#    print alleles
+    if bool(alleles):
+        valkey = [(alleles[k],k) for k in alleles]
+        valkey.sort()
+#        print rec.POS,alleles, valkey
+        sortkeys = [k for (v,k) in valkey]
+        if rec.REF in sortkeys:
+            sortkeys.remove(rec.REF) #deal only with alt calls
+    
+        #make call from ref+most common alt
+        if len(sortkeys) > 0:
+            realCall = rec.REF+"/"+sortkeys[0]
+        else:
+            realCall = rec.REF+"/"+rec.REF
+        if var in fakecall:
+            if realCall == fakecall[var]:
+                conc = True
+        else:
+            conc=None
             
-    if var in fakecall:
-        conc=False
-        for alt in rec.ALT:
-            alleles = rec.REF+"/"+str(alt)
-#            print "THIS",var, fakecall[var], alleles
-            if fakecall[var] == alleles:
-                conc=True
+
+
+    if rec.is_snp: 
+        rec.INFO['type']='SNP'
     else: 
-        pass
-#        print rec.CHROM,rec.POS,"!"
+        rec.INFO['type']='INDEL'
 
-    for call in rec.samples:
-        sample, lane = call.sample.split('_')
-        calls[(sample,int(lane))] = call.gt_type
-
-#    samples = list(set([s for s,l in calls]))
-    for s in samples:
-        if (s,1) not in calls: calls[(s,1)] = None
-        if (s,2) not in calls: calls[(s,2)] = None
-
-        if calls[(s,1)] is None and calls[(s,2)] is None:
-            calls[(s,-1)] = "."
-        elif calls[(s,1)] is None:
-#            calls[(s,-1)] = str(calls[(s,2)])+"*"
-            calls[(s,-1)] = str(calls[(s,2)])
-        elif calls[(s,2)] is None:        
-#            calls[(s,-1)] = str(calls[(s,1)])+"*"
-            calls[(s,-1)] = str(calls[(s,1)])
-        elif calls[(s,1)] == calls[(s,2)]:
-            calls[(s,-1)] = str(calls[(s,1)])
+    #GET COUNTS OF WHICH FILTERS REMOVE DISCORDANT ALLELES
+    if len(rec.FILTER) == 0:
+        if ('PASS',rec.INFO['type'],conc) not in fCounts: 
+            fCounts[('PASS',rec.INFO['type'],conc)] = 0
+        fCounts[('PASS',rec.INFO['type'],conc)] += 1
+   
+    else:
+        for f in rec.FILTER:
+            if (f,rec.INFO['type'],conc) not in fCounts: 
+                fCounts[(f,rec.INFO['type'],conc)] = 0
+            fCounts[(f,rec.INFO['type'],conc)] += 1
+           
+    if args.filter and conc is None:
+        if len(rec.FILTER) == -1:
+            rec.FILTER[1]=args.prefix+'refAbsent'
         else:
-            calls[(s,-1)] = "!!"
-
-    #calculate call consistency
-    dd2call=set()
-    allCall=True
-    for s in DD2s:
-        if calls[(s,-1)] != ".":
-            dd2call.add(calls[(s,-1)])
+            rec.FILTER += [args.prefix+'RefAbsent']
+    elif args.filter and not conc:
+        if len(rec.FILTER) == -1:
+            rec.FILTER[1]=args.prefix+'Discord'
         else:
-            allCall=False
-    cons=None
-    #all called only if all calls are the same
-    if cons is not None:
-        allCall = (allCall & cons)
-    if len(dd2call)==1: cons=True
-    elif len(dd2call)>1: cons=False
-
-#    if rec.is_indel:
-
-
-    print "\t".join([rec.CHROM,str(rec.POS),str(len(rec.alleles)),rec.var_subtype,str(conc),str(cons),str(allCall)]+
-                        [calls[(s,-1)] for s in samples])
-    if cons is not None: rec.INFO['DD2CONS']=cons
-    if conc is not None: rec.INFO['DD2CONC']=conc
-    if allCall is not None: rec.INFO['DD2ALL']=allCall
-    if rec.is_snp: rec.INFO['type']='SNP'
-    else: rec.INFO['type']='INDEL'
-#    rec.INFO['DD2CONS']=str(cons)
-#    rec.INFO['DD2CONC']=str(conc)
-#    rec.INFO['DD2ALL']=str(allCall)
+            rec.FILTER += [args.prefix+'Discord'] 
+        
     vcfout1.write_record(rec)
 
+filters = [f for f,t,c in fCounts]
+filters = list(set(filters))
+filters.sort()
+print "\t".join(["FILTER","S_CONC","S_DISC","S_MISS","I_CONC","I_DISC","I_MISS"])
+for f in filters:
+    if (f,'SNP',True) not in fCounts: fCounts[(f,'SNP',True)]=0
+    if (f,'SNP',False) not in fCounts: fCounts[(f,'SNP',False)]=0
+    if (f,'INDEL',True) not in fCounts: fCounts[(f,'INDEL',True)]=0
+    if (f,'INDEL',False) not in fCounts: fCounts[(f,'INDEL',False)]=0
+
+    print "\t".join(map(str,[f, fCounts[(f,'SNP',True)],fCounts[(f,'SNP',False)],fCounts[(f,'SNP',None)],
+                    fCounts[(f,'INDEL',True)],fCounts[(f,'INDEL',False)],fCounts[(f,'INDEL',None)]]))
+                    
+    
+    
