752ef6e541000cbb76f725e4ed66eb83e0bb0cea
Seth Redmond <sredmond@node1749.broadinstitute.org>
2015-10-06 18:47:00 -0400

    making all-diploid 012 file

--- analyses/wetlab_validation/plotHaplotypes.sh
+++ analyses/wetlab_validation/plotHaplotypes.sh
@@ -5,6 +5,6 @@ VCF=$1
 $DISCO1/scripts/diploidifyVCF.py -v $VCF
 VCF=${VCF/.gz/}
 
-vcftools --012 --vcf ${VCF/.vcf/.HAPLOID.vcf} --out ${VCF/.vcf/}
+vcftools --012 --vcf ${VCF/.vcf/.DIPLOID.vcf} --out ${VCF/.vcf/}
 
 #Rscript $DISCO1/scripts/R/plotHaplotypes.R ${VCF/.vcf/.012/} ${VCF/.vcf/.012.indv/}  ${VCF/.vcf/.012.pos/}
