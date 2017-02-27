00d490c3f2b288e2f2e8c22728aeb8e581ef3be4
sredmond <sredmond@hw-uger-1003.broadinstitute.org>
2017-02-27 18:45:23 -0500

    adding bootstrap pars tree builder

--- analyses/TREES/pars_trees/getParsTreePedBOOT.R
+++ analyses/TREES/pars_trees/getParsTreePedBOOT.R
@@ -1,17 +1,28 @@
-library(ggplot2)
+#library(ggplot2)
 library(ape)
 library(stringdist)
-library(reshape2)
+#library(reshape2)
 library(phangorn)
+
+
+sym <- function(M) {
+  M[lower.tri(M)] = t(M)[lower.tri(M)]
+  M
+}
+
 
 
 fileroot<-"Thies_all_manual.PASS.Cls.miss0.5.LMRG.HAP.alleles"
 fileroot <- commandArgs(TRUE)[1]
 
 tab=paste(fileroot,'tab',sep='.')
-outpng=paste(fileroot,'png',sep='.')
-outnex=paste(fileroot,'nexus',sep='.')
-outtab=paste(fileroot,'dist.tab.txt',sep='.')
+boot <- Sys.getenv('SGE_TASK_ID')
+
+ped=paste(fileroot,'ped',sep='.')
+outpng=paste(fileroot,boot,'png',sep='.')
+outnex=paste(fileroot,boot,'pars.nexus',sep='.')
+outtab=paste(fileroot,boot,'dist.tab.txt',sep='.')
+
 
 #read in ped file and get hamming distances:
 alleleTab <- read.table(tab,colClasses="character",header=T,na.strings = c("."))
@@ -20,10 +31,16 @@ genos <- t(data.matrix(alleleTab[6:dim(alleleTab)[2]]))
 inds <- row.names(genos)
 
 
+noGenos = dim(genos)[[2]]
+sample <- sample(1:noGenos,replace=T)
+
+#bootstrap to full size of genotype matrix:
+genos <- genos[,sample]
+
 genosDat <- as.phyDat(genos, type="USER", levels = c(0:max(genos,na.rm=T)))
 
-distmat <- dist.hamming(genosDat,exclude="pairwise")
-treeNJ <- NJ(distmat)
+distmat <- dist.hamming(genosDat)
+#treeNJ <- NJ(distmat)
 
 #
 #distmat2 <- matrix(nrow=length(inds),ncol=length(inds))
@@ -40,7 +57,7 @@ treeNJ <- NJ(distmat)
 
 
 #get parsinomy score
-parsimony(treeNJ,genosDat)
+#parsimony(treeNJ,genosDat)
 
 #get parsinomy tree
 #treePars <- optim.parsimony(treeNJ,genosDat)
@@ -48,11 +65,14 @@ write("getting parsimony tree",stderr())
 treeRatchet <- pratchet(genosDat)
 write(str(treeRatchet))
 #re-add distances
-#write.table(as.matrix(distmat),stderr())
+write(dim(as.matrix(distmat)),stderr())
+write.table(round(as.matrix(distmat),2),stderr())
+#write(treeRatchet$tip.label,stderr())
+
 
 write("re-adding distances",stderr())
-write(paste("symmetric",isSymmetric(as.matrix(distmat))),stderr())
-treeRatchet <- nnls.phylo(treeRatchet,distmat)
+treeRatchet <- nnls.phylo(treeRatchet,as.matrix(distmat),rooted=T)
+#treeRatchet <- nnls.phylo(treeRatchet,distmat)
 
 #write(treeRatchet,stderr())
 
